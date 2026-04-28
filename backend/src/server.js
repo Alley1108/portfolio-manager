91398e064ad4c714b49a49bd3955ca231b1fb316
Alley1108 <viraneyali@gmail.com>
2026-04-28 16:04:14 +0530

    Auto create DB tables on startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const initDB = require('./models/init');
 
 dotenv.config();
 
@@ -8,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/clients', require('./routes/clients'));
 app.use('/api/stocks', require('./routes/stocks'));
@@ -19,7 +19,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await initDB();
   console.log(`Server running on port ${PORT}`);
 });
 
